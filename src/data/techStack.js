27ede6d883f2199d4035773fc0860a1945a3f20c
= <=>
2026-01-19 20:35:13 -0500

    Update to set it online with gh-pages

--- src/data/techStack.js
+++ src/data/techStack.js
@@ -1,87 +1,105 @@
+import ReactIcon from '@assets/tech/React.png';
+import NodeIcon from '@assets/tech/Nodejs.png';
+import TailwindIcon from '@assets/tech/TailwindCSS.png';
+import PostgresIcon from '@assets/tech/PostgresSQL.png';
+import CSharpIcon from '@assets/tech/CSharp.png';
+import UnityIcon from '@assets/tech/Unity.png';
+import TypeScriptIcon from '@assets/tech/TypeScript.png';
+import GitHubIcon from '@assets/tech/GitHub.png';
+import CSSIcon from '@assets/tech/CSS3.png';
+import RedisIcon from '@assets/tech/Redis.png';
+import JavaScriptIcon from '@assets/tech/JavaScript.png';
+import NestJSIcon from '@assets/tech/Nestjs.png';
+import MySQLIcon from '@assets/tech/MySQL.png';
+import APIIcon from '@assets/tech/api.png';
+import ViteIcon from '@assets/tech/Vitejs.png';
+import PostmanIcon from '@assets/tech/Postman.png';
+import SwiftIcon from '@assets/tech/Swift.png';
+
 export const techStack = {
     react: {
         name: "React",
         category: "frontend",
-        iconPath: "src/assets/tech/React.png"
+        iconPath: ReactIcon
     },
     node: {
         name: "Node.js",
         category: "backend",
-        iconPath: "src/assets/tech/Nodejs.png"
+        iconPath: NodeIcon
     },
     tailwind: {
         name: "Tailwind CSS",
         category: "styling",
-        iconPath: "src/assets/tech/TailwindCSS.png"
+        iconPath: TailwindIcon
     },
     postgresql: {
         name: "PostgresSQL",
         category: "database",
-        iconPath: "src/assets/tech/PostgresSQL.png"
+        iconPath: PostgresIcon
     },
     cSharp: {
         name: "C#",
         category: "programming-language",
-        iconPath: "src/assets/tech/CSharp.png"
+        iconPath: CSharpIcon
     },
     unity: {
         name: "Unity",
         category: "game-engine",
-        iconPath: "src/assets/tech/Unity.png"
+        iconPath: UnityIcon
     },
     typescript: {
         name: "TypeScript",
         category: "programming-language",
-        iconPath: "src/assets/tech/TypeScript.png"
+        iconPath: TypeScriptIcon
     },
     github: {
         name: "GitHub",
         category: "version-control",
-        iconPath: "src/assets/tech/GitHub.png"
+        iconPath: GitHubIcon
     },
     css: {
         name: "CSS",
         category: "styling",
-        iconPath: "src/assets/tech/CSS3.png"
+        iconPath: CSSIcon
     },
     redis: {
         name: "Redis",
         category: "database",
-        iconPath: "src/assets/tech/Redis.png"
+        iconPath: RedisIcon
     },
     javascript: {
         name: "JavaScript",
         category: "programming-language",
-        iconPath: "src/assets/tech/JavaScript.png"
+        iconPath: JavaScriptIcon
     },
     nestjs: {
         name: "NestJS",
         category: "backend",
-        iconPath: "src/assets/tech/Nestjs.png"
+        iconPath: NestJSIcon
     },
     mysql: {
         name: "MySQL",
         category: "database",
-        iconPath: "src/assets/tech/MySQL.png"
+        iconPath: MySQLIcon
     },
     restApi: {
         name: "REST API",
         category: "backend",
-        iconPath: "src/assets/tech/api.png"
+        iconPath: APIIcon
     },
     vite: {
         name: "Vite.js",
         category: "build-tool",
-        iconPath: "src/assets/tech/Vitejs.png"
+        iconPath: ViteIcon
     },
     postman: {
         name: "Postman",
         category: "api-testing",
-        iconPath: "src/assets/tech/Postman.png"
+        iconPath: PostmanIcon
     },
     swiftUI: {
         name: "SwiftUI",
         category: "frontend",
-        iconPath: "src/assets/tech/Swift.png"
+        iconPath: SwiftIcon
     },
 };
